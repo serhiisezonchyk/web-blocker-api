27c146344108604fc5bbc50d477845d6d8a99c03
serhiisezonchyk <sergey040901@gmail.com>
2024-10-25 18:18:13 +0300

    feat: add Auth Guasrd

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,11 +7,13 @@ import {
   HttpStatus,
   Post,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { GetSessionInfoDto, SignInBodyDto, SignUpBodyDto } from './dto';
 import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 import { Response } from 'express';
 import { CookieService } from './cookie.service';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -51,12 +53,14 @@ export class AuthController {
   @Post('sign-out')
   @ApiOkResponse()
   @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard)
   signOut() {}
 
   @Get('session')
   @ApiOkResponse({
     type: GetSessionInfoDto,
   })
+  @UseGuards(AuthGuard)
   getSessionInfo() {
     return null;
   }
