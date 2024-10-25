52f5344b20e58b1d86653491a1dd769c8ddf0a83
serhiisezonchyk <sergey040901@gmail.com>
2024-10-25 15:46:03 +0300

    feat: connect db

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+import { DbService } from './db/db.service';
+class HelloWorldDTO {
+  @ApiProperty()
+  message: string;
+}
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private dbService: DbService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: HelloWorldDTO,
+  })
+  async getHello(): Promise<HelloWorldDTO> {
+    const users = await this.dbService.user.findMany();
+    console.log(users);
+    return { message: 'wdfwed' };
   }
 }
